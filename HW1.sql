6ce7c68799472132743d4a4d7576a831855ab247
Aleksandr <platunov.aleksandr@mail.ru>
2023-06-12 16:08:44 +0300

    ДЗ №1

--- HW1.sql
+++ HW1.sql
@@ -1,7 +1,7 @@
+# 1. Задание
 USE SQL_HW;
-
-CREATE TABLE mobile_phone
-/*
+DROP TABLE IF EXISTS mobile_phone;
+CREATE TABLE IF NOT EXISTS mobile_phone
 (
 	Id INT AUTO_INCREMENT PRIMARY KEY,
     ProductName VARCHAR(30) NOT NULL,
@@ -9,7 +9,6 @@ CREATE TABLE mobile_phone
     ProductCount INT DEFAULT 0,
     Price DECIMAL
 );
-SELECT * FROM mobile_phone;
 
 INSERT INTO mobile_phone (ProductName, Manufacturer, ProductCount, Price)
 VALUES 
@@ -20,19 +19,24 @@ VALUES
 ('P20 Pro', 'Huawei', 5, 36000);
 
 SELECT * FROM mobile_phone;
-*/
-(
-	Id INT AUTO_INCREMENT PRIMARY KEY,
-    ProductName VARCHAR(30) NOT NULL,
-    Manufacturer VARCHAR(20) NOT NULL,
-    ProductCount INT DEFAULT 0,
-    Price DECIMAL
-);
-INSERT INTO mobile_phone (ProductName, Manufacturer, ProductCount, Price)
-VALUES
-('iPhone X', 'Apple', 3, 76000),
-('iPhone 8', 'Apple', 2, 51000),
-('Galaxy S9', 'Samsung', 2, 56000),
-('Galaxy S8', 'Samsung', 1, 41000),
-('P20 Pro', 'Huawei', 5, 36000);
-SELECT * FROM mobile_phone;
+
+# 2. Задание
+SELECT ProductName, Manufacturer, Price FROM mobile_phone
+WHERE ProductCount > 2;
+
+# 3. Задание
+SELECT * FROM mobile_phone
+WHERE Manufacturer = 'Samsung';
+
+# 4. Задание (чет сразу решил проверять по дву столбикам)
+# 4.1 
+SELECT * FROM mobile_phone
+WHERE ProductName LIKE 'iP%'  OR  Manufacturer LIKE 'iP%';
+
+# 4.2
+SELECT * FROM mobile_phone
+WHERE ProductName LIKE 'Sams___'  OR  Manufacturer LIKE 'Sams___';
+
+# 4.3
+SELECT * FROM mobile_phone
+WHERE ProductName LIKE '%8'  OR  Manufacturer LIKE '%8';
